76697a918ffe81e5e893227b7d5032c0c708f01a
Pedro Gabriel Lima <pgsl@ecomp.poli.br>
2022-09-01 16:57:59 -0300

    Implementacao de API, metodo POST, comunicacao com o front, fetch

---
+++ HTTPserver/request.py
@@ -0,0 +1,30 @@
+import re
+from xml.etree.ElementTree import tostring
+
+class Request():
+    def __init__(self,method,path,headers,body):
+        self.method=method
+        self.path=path
+        self.headers=headers
+        self.body=body
+
+
+    def __str__(self):
+        return "method: %s\npath:%s\nheaders: %s\nbody: %s"%(self.method,self.path,self.headers,self.body)
+
+
+    def builder(http_post):
+        req_line = re.compile(r'(?P<method>GET|POST)\s+(?P<resource>.+?)\s+(?P<version>HTTP/1.1)')
+        field_line = re.compile(r'\s*(?P<key>.+\S)\s*:\s+(?P<value>.+\S)\s*')
+        first_line_end = http_post.find('\n')
+        headers_end = http_post.find('\r\n\r\n')
+        request = req_line.match(
+            http_post[:first_line_end]
+        ).groupdict()
+        headers = dict(
+            field_line.findall(
+                http_post[first_line_end:headers_end]
+            )
+        )
+        body = http_post[headers_end + 2:]
+        return Request(request["method"], request["resource"], headers, body)
--- HTTPserver/server.py
+++ HTTPserver/server.py
@@ -1,64 +1,124 @@
 import socket
+import json
+import os
+
+from request import Request
 #import base64
 
-h = open('raiz/index.htm', 'r')
-homepage = h.read()
-i= open('paginateste.html', 'r')
-paginaIpForadaLista = i.read()
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print("-------------------------------------")
-print("Printando servidor",server)
-print("-------------------------------------")
-porta=80
+h = open('raiz/notFound.html', 'r')
+notfound = h.read()
 
-a =server.bind(('', porta))
-print("Binding: ", a)
-print("-------------------------------------")
-b = server.listen(5)
-print("server listen: ", b)
-print("-------------------------------------")
+
+states_list=['add_user_ip','remove_ip','ip_prefix','port']
+website_dict = {}
+lista_ips=['127.0.0.1','192.169.1.10']
+# Lista bruta com todos os arquivos que serão adicionados
+# Ao servidor, daqui eles serão transformados para dict
+allFiles = {}
+
+
+def readFile(file_name):
+    if file_name in allFiles.keys():
+        path= allFiles[file_name]
+        i= open(path, 'r')
+        content= i.read()
+        i.close()
+        return content
+
+def getAllFiles(dir):
+    for r, d, f in os.walk(dir):
+        for file in f:
+            if '.html' in file:
+                allFiles[file.replace('.html','')]=os.path.join(r, file)
+
+
+def encontrarArquivos():
+    for file in allFiles:
+        file = str(file[len(abs_path)+1:])
+        format = file[file.rfind('.')+1:]
+        if format in ['css']:
+            data = open(file, 'r').read()
+            resp = f'HTTP/1.1 200 OK\r\nContent-Type: text/css\r\nContent-Length: {len(data)}\r\n\r\n'
+            website_dict[file] = str.encode(resp + data)
+
+
+def loadHtml(web_socket,path, ip_do_cara):
+    if ip_do_cara in lista_ips:
+        response = bytes(readFile(path), 'utf-8')
+    else:
+        response = bytes(readFile('blocked'), 'utf-8')
+    send_message(web_socket,'text/html', response)
+
+
+def send_message(web_socket, content_type, response):
+    resp = f'HTTP/1.1 200 OK\r\nContent-Type: {content_type}\r\nContent-Length: {len(response)}\r\n\r\n'
+    web_socket.sendall(str.encode(resp) + response)
+
+    
 try:
+    abs_path = os.getcwd()
+    getAllFiles(abs_path)
+    print('--------------------')
+    print('All files: ',allFiles)
+    print('--------------------')
+    welcome_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    nreceive = True#nreceive = Not Received
+    #ticks = 0
+    #f = None
+    #while nreceive and ticks < 101:#try to get the info 100 times or until it's received
+    #    ticks+=1
+    #    try:
+    #        f = welcome_socket.makefile('rb')
+    #        if not f == None:
+    #            nreceive = False
+    #    except:
+    #        pass
+    #data = f.read(1024)
+    welcome_socket.bind(('', 8080))
+    welcome_socket.listen(5)
+    print('Server online')
     while True:
-        ws, addr = server.accept()
-        data = ws.recv(2000)
-        
-        P = data.split(b' ') #GET / HTTP/1.0 -> [GET, /, HTTP/1.0]
-        print("Printando data: ",P)
-        if(addr[0] == "192.168.1.4"):
-            if P[0] == b'GET':
-                print("P1[0]: -",P[0])
-                if P[1] == b'/':
-                    print("P1[1]: -",P[1])
-                    resp = ('HTTP/1.1 200 OK\r\n' + 'Content-Type: text/html\r\n' + 'Content-Length: ' + str(len(homepage)) + '\r\n\r\n' + (homepage))
-                    #resp = ('HTTP/1.0 200 OK\r\n' + 'Content-Type: text/html\r\n' + 'Content-Length: ' + str(len(homepage)) + '\r\n\r\n' + (homepage))
-                    resp = str.encode(resp)
-                    ws.sendall(resp)
-                    print("IP DE QUEM CHEGA:", addr[0])
-                # else:
-                    # ext = str(P[1].rpartition(b'.')[-1])
-                    # f = open(P[1][1:], 'rb')
-                    # figure = f.read()
-                    
-                    # ws.sendall('HTTP/1.0 200 OK\r\n' +
-                                # 'Content-Type: image' + ext + '\r\n' +
-                                # 'Content-Length: ' + str(len(figure)) + '\r\n\r\n' +
-                                # figure)
-        else:
-            if P[0] == b'GET':
-                print("********************************")
-                print("PEGANDO O P1[0]: -",P[0])
-                print("********************************")
-                if P[1] == b'/':
-                    print("====================================")
-                    print("PEGANDO O P1[1]: -",P[1])
-                    print("====================================")
-                    resp = ('HTTP/1.1 200 OK\r\n' + 'Content-Type: text/html\r\n' + 'Content-Length: ' + str(len(paginaIpForadaLista)) + '\r\n\r\n' + (paginaIpForadaLista))
-                    #resp = ('HTTP/1.0 200 OK\r\n' + 'Content-Type: text/html\r\n' + 'Content-Length: ' + str(len(homepage)) + '\r\n\r\n' + (homepage))
-                    resp = str.encode(resp)
-                    ws.sendall(resp)
+        print('ITEROU 000000')
+        web_socket, _ = welcome_socket.accept()
+        print('KKKKKKKKKKKKKKKKKKKKK')
+        print('ACCEPTED: ', web_socket)
+        print('KKKKKKKKKKKKKKKKKKKKK')
+        #data = web_socket.recv(2000).split(b'\r\n')
+        webscoket_decoder=web_socket.recv(2000).decode()
+        print('WEBSCOKET_DECODER: ', webscoket_decoder)
+        request_data=Request.builder(webscoket_decoder)
+        print('==============================')
+        print('Imprimindo data: ', request_data)
+        print('==============================')
+        if request_data.method == 'GET':
+            print('----------------------------------')
+            client_IP = web_socket.getpeername()[0]
+            print('IP: ', client_IP)
+            print('----------------------------------')
+            
+
+            # Requests padrões já armazenadas no website_dict
+            #################################################
+            # Alternativas caso os arquivos de mesmo caminho não sejam encontrados
+            path_matches = list(filter(lambda filename : filename==request_data.path.replace('/',''), allFiles.keys()))
+            #################################################  
+            print('--------------------')
+            print('Path_matches: ',path_matches)
+            print('--------------------')
+            if len(path_matches) > 0:
+                loadHtml(web_socket, path_matches[0], client_IP)
+            elif states_list in path_matches:
+                #validar se eh GET. Faz sentido se for GET.
+                print('A FAZER')
+            else:
+                loadHtml(web_socket, 'notFound', client_IP)
+        elif request_data.method== 'POST':
+            if request_data.path.replace('/', '') in states_list:
+                #criar filtro, como if, para mandar coisas diferentes de 'ip_changed'
+                send_message(web_socket, 'text/plain', str.encode('ip_changed'))
+
 except KeyboardInterrupt:
     print(" terminado pelo usuario")
-    ws.close()
-    server.close()
-
+    web_socket.close()
+    welcome_socket.close()
 # sudo fuser -i -k 80/tcp
